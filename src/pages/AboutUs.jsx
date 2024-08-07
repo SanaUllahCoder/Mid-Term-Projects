24ef34984b9c5eedc6661de7bd701ee7693266bd
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2024-08-07 15:49:04 +0500

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './AboutUs.css';
+import aboutImage from '../assets/about-image.jpg';
 
 const AboutUs = () => {
     return (
         <div className="about-us">
+            <img src={aboutImage} alt="About Us" className="about-image" />
             <h1>About Us</h1>
             <p>Welcome to HOTELIER! We have been providing the best hotel services since 1990.</p>
             <p>Our mission is to ensure our guests have the most comfortable and enjoyable stay possible. We offer a variety of rooms, top-notch amenities, and exceptional customer service.</p>
